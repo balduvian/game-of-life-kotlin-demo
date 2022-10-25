82518a64261045440dbe634cbe4f58c87c81d3e0
balduvian <emmettglaser@gmail.com>
2022-10-25 14:01:55 -0700

    advanced life simulation

--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -1,48 +1,419 @@
 import kotlin.random.Random
 
+/* 0 0 0 0 | A */
+/* 0 0 1 0 | A */
+/* 0 1 0 0 | A */
+/* 0 1 1 0 | A */
+
+/* 0 0 0 1 | B */
+/* 0 1 0 1 | B */
+/* 1 0 0 1 | B */
+/* 1 1 0 1 | B */
+
+/* 0 0 1 1 | C */
+/* 0 1 1 1 | C */
+/* 1 0 1 1 | C */
+/* 1 1 1 1 | C */
+
+/* 1 0 0 0 | D */
+/* 1 0 1 0 | D */
+/* 1 1 0 0 | D */
+/* 1 1 1 0 | D */
+
+/** ----------------------- */
+
+/* 0 0 0 0 | 0 */
+
+/* 0 0 1 0 | 1 */
+/* 0 1 0 0 | 1 */
+/* 0 0 0 1 | 1 */
+/* 1 0 0 0 | 1 */
+
+/* 0 1 1 0 | 2 */
+/* 0 1 0 1 | 2 */
+/* 1 0 0 1 | 2 */
+/* 0 0 1 1 | 2 */
+/* 1 0 1 0 | 2 */
+/* 1 1 0 0 | 2 */
+
+/* 1 1 0 1 | 3 */
+/* 0 1 1 1 | 3 */
+/* 1 0 1 1 | 3 */
+/* 1 1 1 0 | 3 */
+
+/* 1 1 1 1 | 4 */
+
+/** ----------------------- */
+
+/* 0 0 0 0 | A */
+/* 0 0 0 1 | B */
+/* 0 0 1 0 | A */
+/* 0 0 1 1 | C */
+/* 0 1 0 0 | A */
+/* 0 1 0 1 | B */
+/* 0 1 1 0 | A */
+/* 0 1 1 1 | C */
+/* 1 0 0 0 | D */
+/* 1 0 0 1 | B */
+/* 1 0 1 0 | D */
+/* 1 0 1 1 | C */
+/* 1 1 0 0 | D */
+/* 1 1 0 1 | B */
+/* 1 1 1 0 | D */
+/* 1 1 1 1 | C */
+
+/*
+ * actions
+ * 0A: attack
+ * 1B: defend
+ * 2C: heal
+ * 3D: spawn
+ */
+
+/*
+ * 0 0 0 0 | header
+ * 0 0 0 0 | value
+ * 0 0 0 0 | header
+ * 0 0 0 0 | value
+ * 0 0 0 0 | header
+ * 0 0 0 0 | value
+ * 0 0 0 0 | header
+ * 0 0 0 0 | value
+ */
+
+object BitUtil {
+	fun get(code: Int, offset: Int, length: Int): Int {
+		return code.ushr(offset).and(0x1.shl(length) - 1)
+	}
+
+	fun set(code: Int, value: Int, offset: Int, length: Int): Int {
+		val max = (0x1.shl(length) - 1)
+		return code.and(max.shl(offset).inv()).or(
+			value.coerceIn(0..max).shl(offset)
+		)
+	}
+}
+
+/**
+ * 0 | alive
+ * ...
+ * 0 0 0 0 0 0 0 0 | life
+ * 0 0 0 0 0 0 0 0 | age
+ */
+object State {
+	const val START_LIFE = 4
+	const val MAX_LIFE = 32
+	const val DEAD_AGE = 64
+
+	fun getAlive(code: Int): Boolean {
+		return BitUtil.get(code, 31, 1) == 1
+	}
+
+	fun getLife(code: Int): Int {
+		return BitUtil.get(code, 8, 8)
+	}
+	fun setLife(code: Int, life: Int): Int {
+		return BitUtil.set(code, life, 8, 8)
+	}
+
+	fun getAge(code: Int): Int {
+		return BitUtil.get(code, 0, 8)
+	}
+	fun setAge(code: Int, age: Int): Int {
+		return BitUtil.set(code, age, 0, 8)
+	}
+
+	fun initial(): Int {
+		return START_LIFE.shl(8).or(0).or(1.shl(31))
+	}
+}
+
+object Organism {
+	const val ACTION_ATTACK = 0
+	const val ACTION_DEFEND = 1
+	const val ACTION_HEAL = 2
+	const val ACTION_SPAWN = 3
+
+	const val FAMILY_THRESHOLD = 0.75
+
+	val headerMap = intArrayOf(
+		/* 0 0 0 0 | A */ 0,
+		/* 0 0 0 1 | B */ 1,
+		/* 0 0 1 0 | A */ 0,
+		/* 0 0 1 1 | C */ 2,
+		/* 0 1 0 0 | A */ 0,
+		/* 0 1 0 1 | B */ 1,
+		/* 0 1 1 0 | A */ 0,
+		/* 0 1 1 1 | C */ 2,
+		/* 1 0 0 0 | D */ 3,
+		/* 1 0 0 1 | B */ 1,
+		/* 1 0 1 0 | D */ 3,
+		/* 1 0 1 1 | C */ 2,
+		/* 1 1 0 0 | D */ 3,
+		/* 1 1 0 1 | B */ 1,
+		/* 1 1 1 0 | D */ 3,
+		/* 1 1 1 1 | C */ 2,
+	)
+
+	fun getHeader(code: Int, slot: Int) = headerMap[BitUtil.get(code, 8 * slot + 4, 4)]
+	fun getTrigger(code: Int, slot: Int) = BitUtil.get(code, 8 * slot, 4).countOneBits() + 3
+
+	fun getActions(code: Int, neighbors: Int): ArrayList<Int> {
+		val actions = ArrayList<Int>(4)
+		for (i in 0 until 4) {
+			val action = getHeader(code, i)
+
+			if (action != ACTION_SPAWN && neighbors == getTrigger(code, i))
+				actions.add(action)
+		}
+		return actions
+	}
+
+	fun canSpawn(code: Int, neighbors: Int): Boolean {
+		for (i in 0 until 4) {
+			val action = getHeader(code, i)
+			if (action == ACTION_SPAWN && neighbors == getTrigger(code, i)) return true
+		}
+		return false
+	}
+
+	fun getColor(code: Int): Int {
+		val c = getColorChannel(code, 0)
+		val m = getColorChannel(code, 1)
+		val y = getColorChannel(code, 2)
+
+		val r = (0xff * c).toInt()
+		val g = (0xff * m).toInt()
+		val b = (0xff * y).toInt()
+
+		return 0xFF000000.toInt().or(r.shl(16)).or(g.shl(8)).or(b)
+	}
+
+	fun getColorChannel(code: Int, channel: Int): Float {
+		val top = BitUtil.get(code, 8 * channel + 4, 4)
+		val bottom = BitUtil.get(code, 8 * channel, 4)
+
+		return (
+			(top.ushr(3).and(0x1) + bottom.ushr(3).and(0x1)) * 27 +
+			(top.ushr(2).and(0x1) + bottom.ushr(2).and(0x1)) * 9 +
+			(top.ushr(1).and(0x1) + bottom.ushr(1).and(0x1)) * 3 +
+			(top.and(0x1) + bottom.and(0x1))
+		) / 80.0f
+	}
+
+	fun sameFamily(first: Int, second: Int): Boolean {
+		return first.xor(second).inv().countOneBits() >= (FAMILY_THRESHOLD * 32).toInt()
+	}
+
+	/** @return indices */
+	fun findSimilarPair(codes: List<Int>): Pair<Int, Int>? {
+		if (codes.size < 2) return null
+
+		val options = Array(codes.size) { i ->
+			val list = ArrayList<Int>()
+
+			for (j in codes.indices) {
+				if (i == j) continue
+				if (sameFamily(codes[i], codes[j])) list.add(j)
+			}
+
+			list
+		}
+
+		val indices = codes.indices.shuffled()
+		for (i in indices.indices) {
+			val firstIndex = indices[i]
+			if (options[firstIndex].isEmpty()) continue
+
+			return firstIndex to options[firstIndex].random()
+		}
+
+		return null
+	}
+
+	fun breed(parent0: Int, parent1: Int, mutationRate: Float): Int {
+		var newCode = 0
+
+		for (i in 0 until 28) {
+			var bit = (if (Random.nextBoolean()) parent0 else parent1).shr(i).and(0x1)
+			if (Random.nextFloat() < mutationRate) bit = bit.inv().and(0x1)
+			newCode = newCode.or(bit.shl(i))
+		}
+
+		return newCode
+	}
+}
+
 class Board(val width: Int, val height: Int) {
-	var grid = IntArray(width * height)
+	companion object {
+		val offsets = arrayOf(
+			-1 to  0,
+			 0 to -1,
+			 1 to  0,
+			 0 to  1,
+			-1 to -1,
+			 1 to  1,
+			-1 to  1,
+			 1 to -1,
+		)
+	}
 
-	fun mod(a: Int, b: Int) = ((a % b) + b) % b
+	val genomeGrid = arrayOf(IntArray(width * height), IntArray(width * height))
+	val stateGrid = arrayOf(IntArray(width * height), IntArray(width * height))
 
-	fun accessDirect(x: Int, y: Int) = grid[y * width + x]
+	val health = IntArray(width * height)
 
-	fun access(x: Int, y: Int) = grid[mod(y, height) * width + mod(x, width)]
+	var phase = 0
+	fun otherPhase() = if (phase == 0) 1 else 0
+
+	inline fun getGenome() = genomeGrid[phase]
+	inline fun getState() = stateGrid[phase]
+
+	inline fun mod(a: Int, b: Int) = ((a % b) + b) % b
+
+	inline fun index(x: Int, y: Int) = y * width + x
+
+	inline fun wrapIndex(x: Int, y: Int) = mod(y, height) * width + mod(x, width)
 
 	fun clear() {
+		val genome = getGenome()
+		val state = getState()
+
 		for (i in 0 until width * height) {
-			grid[i] = 0
+			genome[i] = 0
+			state[i] = 0
 		}
 	}
 
 	fun randomPopulate() {
+		val genome = getGenome()
+		val state = getState()
+
 		for (i in 0 until width * height) {
-			grid[i] = if (Random.nextInt(3) == 0) 1 else 0
+			if (Random.nextInt(3) == 0) {
+				genome[i] = Random.nextInt()
+				state[i] = State.initial()
+			} else {
+				genome[i] = 0
+				state[i] = 0
+			}
 		}
 	}
 
 	fun doStep() {
-		val newGrid = IntArray(width * height)
+		val oldGenome = genomeGrid[phase]
+		val newGenome = genomeGrid[otherPhase()]
 
+		val oldState = stateGrid[phase]
+		val newState = stateGrid[otherPhase()]
+
+		/* prepare new buffers */
+		for (i in 0 until width * height) {
+			newGenome[i] = 0
+			newState[i] = 0
+		}
+
+		/* set initial health */
+		for (i in 0 until width * height) {
+			val state = oldState[i]
+			health[i] = if (State.getAlive(state)) State.getLife(state) else 0
+		}
+
+		/* organisms fighting for survival */
 		for (y in 0 until height) {
 			for (x in 0 until width) {
-				val neighbors = access(x - 1, y) +
-					access(x + 1, y) +
-					access(x, y - 1) +
-					access(x, y + 1) +
-					access(x - 1, y - 1) +
-					access(x + 1, y + 1) +
-					access(x - 1, y + 1) +
-					access(x + 1, y - 1)
+				val current = oldGenome[index(x, y)]
+				val state = oldState[index(x, y)]
 
-				newGrid[y * width + x] = if (grid[y * width + x] == 0) {
-					if (neighbors == 3) 1 else 0
-				} else {
-					if (neighbors < 2 || neighbors > 3) 0 else 1
+				if (State.getAlive(state)) {
+					val neighbors = offsets.count { (ox, oy) -> State.getAlive(oldState[wrapIndex(x + ox, y + oy)]) }
+
+					val actions = Organism.getActions(current, neighbors)
+					actions.forEach { action ->
+						when (action) {
+							Organism.ACTION_ATTACK -> {
+								offsets.forEach { (ox, oy) ->
+									if (State.getAlive(oldState[wrapIndex(x + ox, y + oy)])) {
+										if (!Organism.sameFamily(current, oldGenome[wrapIndex(x + ox, y + oy)])) {
+											health[wrapIndex(x + ox, y + oy)] -= 1
+										}
+									}
+								}
+							}
+							Organism.ACTION_DEFEND -> {
+								health[index(x, y)] += 1
+							}
+							Organism.ACTION_HEAL -> {
+								offsets.forEach { (ox, oy) ->
+									if (State.getAlive(oldState[wrapIndex(x + ox, y + oy)])) {
+										if (Organism.sameFamily(current, oldGenome[wrapIndex(x + ox, y + oy)])) {
+											health[wrapIndex(x + ox, y + oy)] += 1
+										}
+									}
+								}
+							}
+						}
+					}
+					/* organism did not like this configuration */
+					if (actions.isEmpty()) {
+						health[index(x, y)] -= 1
+					}
 				}
 			}
 		}
 
-		grid = newGrid
+		/* organisms reproducing */
+		for (y in 0 until height) {
+			for (x in 0 until width) {
+				val state = oldState[index(x, y)]
+
+				if (!State.getAlive(state)) {
+					val neighborhood = offsets.mapNotNull { (ox, oy) ->
+						val index = wrapIndex(x + ox, y + oy)
+
+						if (State.getAlive(oldState[index]))
+							index to oldGenome[index]
+						else
+							null
+					}
+
+					val breeders = neighborhood.filter { (_, genome) -> Organism.canSpawn(genome, neighborhood.size) }
+
+					val pairIndices = Organism.findSimilarPair(breeders.map { (_, genome) -> genome })
+					if (pairIndices != null) {
+						val newOrganism = Organism.breed(
+							neighborhood[pairIndices.first].second,
+							neighborhood[pairIndices.second].second,
+							0.01f
+						)
+
+						newGenome[index(x, y)] = newOrganism
+						newState[index(x, y)] = State.initial()
+						health[neighborhood[pairIndices.first].first] -= 1
+						health[neighborhood[pairIndices.second].first] -= 1
+					}
+				}
+			}
+		}
+
+		/* organisms dying */
+		for (i in 0 until width * height) {
+			/* freshly created organism */
+			if (State.getAlive(newState[i])) continue
+
+			val life = health[i]
+			val state = oldState[i]
+			val newAge = State.getAge(state) + 1
+
+			if (!State.getAlive(state) || life <= 0 || newAge >= State.DEAD_AGE) {
+				newState[i] = 0
+				newGenome[i] = 0
+			} else {
+				newState[i] = State.setLife(State.setAge(state, newAge), life.coerceAtMost(State.MAX_LIFE))
+				newGenome[i] = oldGenome[i]
+			}
+		}
+
+		phase = otherPhase()
 	}
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SnapshotMutationPolicy
@@ -19,6 +20,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 fun main() = application {
 	Window(
@@ -27,7 +29,7 @@ fun main() = application {
 		state = rememberWindowState(width = 640.dp, height = 480.dp),
 	) {
 		val (board, setBoard) = remember {
-			val board = Board(64, 64)
+			val board = Board(128, 128)
 			board.randomPopulate()
 			mutableStateOf(board, object : SnapshotMutationPolicy<Board> {
 				override fun equivalent(a: Board, b: Board) = false
@@ -35,10 +37,11 @@ fun main() = application {
 		}
 		val (going, setGoing) = remember { mutableStateOf(false) }
 		val (step, setStep) = remember { mutableStateOf(0) }
+		val (speed, setSpeed) = remember { mutableStateOf(0.5f) }
 
 		LaunchedEffect(going, step) {
 			if (going) {
-				delay(100)
+				delay((speed * 600).toLong())
 				board.doStep()
 				setStep(step + 1)
 			}
@@ -52,6 +55,15 @@ fun main() = application {
 						setBoard(board)
 					}, enabled = !going) {
 						Text("step")
+					}
+
+					Button(onClick = {
+						for (i in 0 until 100) {
+							board.doStep()
+						}
+						setBoard(board)
+					}, enabled = !going) {
+						Text("superstep")
 					}
 
 					Button(onClick = {
@@ -75,6 +87,8 @@ fun main() = application {
 					}) {
 						Text("clear")
 					}
+
+					Slider(speed, { setSpeed(it) }, valueRange = 0.0f.rangeTo(1.0f), steps = 12)
 				}
 
 				Canvas(modifier = Modifier.fillMaxWidth(1f).aspectRatio(
@@ -94,8 +108,12 @@ fun main() = application {
 						val gridY = (y / cellHeight).toInt()
 
 						if (gridX in 0 until board.width && gridY in 0 until board.height) {
-							val existing = board.grid[gridY * board.width + gridX]
-							board.grid[gridY * board.width + gridX] = if (existing == 1) 0 else 1
+							if (!State.getAlive(board.getState()[board.index(gridX, gridY)])) {
+								board.getState()[board.index(gridX, gridY)] = State.initial()
+								board.getGenome()[board.index(gridX, gridY)] = Random.nextInt()
+							} else {
+								board.getState()[board.index(gridX, gridY)] = 0
+							}
 							setBoard(board)
 						}
 					}
@@ -107,8 +125,12 @@ fun main() = application {
 
 					for (y in 0 until board.height) {
 						for (x in 0 until board.width) {
-							if (board.grid[y * board.width + x] == 1) {
-								drawRect(Color.Black, Offset(x * cellWidth, y * cellHeight), Size(cellWidth, cellHeight))
+							if (State.getAlive(board.getState()[board.index(x, y)])) {
+								drawRect(
+									Color(Organism.getColor(board.getGenome()[board.index(x, y)])),
+									Offset(x * cellWidth, y * cellHeight),
+									Size(cellWidth, cellHeight)
+								)
 							}
 						}
 					}
